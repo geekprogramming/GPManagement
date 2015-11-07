39b0b559beb1ed3bbb1822eb460337bae603a05a
Geek Programming <team.geekprogramming@gmail.com>
2015-11-08 01:38:24 +0700

    Thach
    update Schedule

---
+++ GPManagement/GPManagement/GP.Service/ScheduleService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using GPManagement.GP.Dao;
+
+namespace GPManagement.GP.Service
+{
+    class ScheduleService : IService
+    {
+        ScheduleDao dao = new ScheduleDao();
+
+        public List<object> getAllData(object obj, string sortColumn, bool asc, bool exactFilter)
+        {
+            return dao.getAllData(obj, sortColumn, asc, exactFilter);
+        }
+
+        public bool insertNewData(object obj)
+        {
+            return dao.insertNewData(obj);
+        }
+
+        public bool removeData(object obj)
+        {
+            return dao.removeData(obj);
+        }
+
+        public bool updateData(object obj)
+        {
+            return dao.updateData(obj);
+        }
+    }
+}
