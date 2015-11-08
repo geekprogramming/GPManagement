2189dfb77e033c559d9efd6758392615289a70b5
Geek Programming <team.geekprogramming@gmail.com>
2015-11-08 12:41:20 +0700

    thach
    accout update

---
+++ GPManagement/GPManagement/GP.Entity/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPManagement.GP.Entity
+{
+    class Account
+    {
+        private int idEmp;
+        private string userName;
+        private string passWord;
+        private string strFilter;
+
+        public Account()
+        {
+
+        }
+
+        public Account(int idEmp, string userName, string passWord)
+        {
+            this.idEmp = idEmp;
+            this.userName = userName;
+            this.passWord = passWord;
+        }
+
+        public void setIdEmp(int idEmp)
+        {
+            this.idEmp = idEmp;
+        }
+
+        public void setUserName(string userName)
+        {
+            this.userName = userName;
+        }
+
+        public void setPassWord(string passWord)
+        {
+            this.passWord = passWord;
+        }
+
+        public int getIdEmp()
+        {
+            return this.idEmp;
+        }
+
+        public string getUserName()
+        {
+            return this.userName;
+        }
+
+        public string getPassWord()
+        {
+            return this.passWord;
+        }
+
+        public string getStrFilter()
+        {
+            return this.strFilter;
+        }
+
+        public void setStrFilter(String strFilter)
+        {
+            this.strFilter = strFilter;
+        }
+    }
+}
